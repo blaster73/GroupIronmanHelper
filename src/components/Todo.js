f165c826c1768692454f17ebe9b6ee827fbc2ff9
blaster73 <allenbenjamin@knights.ucf.edu>
2021-08-06 13:22:54 -0400

    more npm changes

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,40 +1,15 @@
 import React from "react";
 
 function Todo(props) {
-  function deleteHandler() {
+  /*function deleteHandler() {
     console.log("Clicked Here");
     console.log(props.text);
-  }
+  }*/
 
   const userAction = async () => {
-
-    /*var express = require('express')
-    var cors = require('cors')
-    var app = express()
-    
-    app.use(cors())
-    
-    app.get('/products/:id', function (req, res, next) {
-      res.json({msg: 'This is CORS-enabled for all origins!'})
-    })
-    
-    app.listen(80, function () {
-      console.log('CORS-enabled web server listening on port 80')
-    })*/
-    
-    /*const response = await fetch('https://services.runescape.com/m=hiscore_oldschool/index_lite.ws?player=Killua_blast', {
-      crossDomain:true,
-      method: 'POST',
-      headers: {'Content-Type':'application/csv'},
-      body: Todo.stringify
-    });*/
-
     fetch('https://services.runescape.com/m=hiscore_oldschool/index_lite.ws?player=Killua_blast')
     .then(response => response.json())
     .then(data => console.log(data));
-
-    //const myCsv = await response.text;
-    //console.log(myCsv);
   }
 
   return (
--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Todo from './components/Todo';
+//import Todo from './components/Todo';
 
 ReactDOM.render(<App />, document.getElementById('root'));
